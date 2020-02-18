f2e6c350fd4617fcd6e0cbb92794bc3f429f9958
jackdo69 <jackie.duc.do@gmail.com>
2020-02-18 17:37:02 +1100

    basic styling for images

--- src/components/Image/ImageItem.js
+++ src/components/Image/ImageItem.js
@@ -5,6 +5,11 @@ const ImageItem = props => {
   return (
     <div className="imageItem">
       <img src={props.src} alt={props.alt} />
+      <div className="utilities">
+        <button><i class="fas fa-heart"></i></button>
+        <button><i class="fas fa-plus"></i></button>
+        <button><i class="fas fa-long-arrow-alt-down"></i></button>
+      </div>
     </div>
   );
 };
