b660da1eebc59603b68b9250d8e5710b72fd3b81
jackdo69 <jackie.duc.do@gmail.com>
2020-02-22 11:06:27 +1100

    basic set up for auth

--- src/pages/Auth/Auth.js
+++ src/pages/Auth/Auth.js
@@ -1,11 +1,114 @@
-import React from 'react';
+import React, { useState, useContext } from "react";
+import "./Auth.css";
 
-import './Auth.css';
+import Button from "../../components/Button/Button";
+import Card from "../../components/Card/Card";
+import Input from "../../components/Input/Input";
+import ErrorModal from "../../components/Modal/ErrorModal";
+import LoadingSpinner from "../../components/LoadingSpinner/LoadingSpinner";
+
+import {
+  VALIDATOR_EMAIL,
+  VALIDATOR_MINLENGTH,
+  VALIDATOR_REQUIRE
+} from "../../Validator/validator";
+
+import { useForm } from "../../hooks/form-hook";
+import { useHttpClient } from "../../hooks/http-hook";
+import { AuthContext } from "../../context/auth-context";
 
 const Auth = () => {
-    return <div className="auth">
-        <h2>Auth page</h2>
-    </div>;
-}
- 
+  const auth = useContext(AuthContext);
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
+  const [isLoginMode, setIsLoginMode] = useState(true);
+  const [formState, inputHandler, setFormField] = useForm(
+    {
+      email: {
+        value: "",
+        isValid: false
+      },
+      password: {
+        value: "",
+        isValid: false
+      }
+    },
+    false
+  );
+
+  const switchModeHandler = () => {
+    if (!isLoginMode) {
+      setFormField({
+        ...formState.inputs,
+        name: {
+          value: "",
+          isValid: false
+        }
+      });
+    } else {
+      setFormField(
+        {
+          ...formState.inputs,
+          name: {
+            value: "",
+            isValid: false
+          }
+        },
+        false
+      );
+    }
+    setIsLoginMode(prevMode => !prevMode);
+  };
+
+  const authSubmitHandler = async event => {};
+
+  return (
+    <React.Fragment>
+      <ErrorModal error={error} onClear={clearError} />
+      <Card className="authentication">
+        {isLoading && <LoadingSpinner asOverlay />}
+        <h3>Login Required</h3>
+        <hr />
+        <form onSubmit={authSubmitHandler}>
+          {!isLoginMode && (
+            <Input
+              element="input"
+              id="name"
+              type="text"
+              label="Your Name"
+              validators={[VALIDATOR_REQUIRE()]}
+              errorText="Please enter a name."
+              onInput={inputHandler}
+            />
+          )}
+
+          <Input
+            element="input"
+            id="email"
+            type="email"
+            label="E-Mail"
+            validators={[VALIDATOR_EMAIL()]}
+            errorText="Please enter a valid email address."
+            onInput={inputHandler}
+          />
+          <Input
+            element="input"
+            id="password"
+            type="password"
+            label="Password"
+            validators={[VALIDATOR_MINLENGTH(6)]}
+            errorText="Please enter a valid password, at least 6 characters."
+            onInput={inputHandler}
+          />
+          <Button type="submit" disabled={!formState.isValid}>
+            {isLoginMode ? "LOGIN" : "SIGNUP"}
+          </Button>
+        </form>
+        <Button inverse onClick={switchModeHandler}>
+          SWITCH TO {isLoginMode ? "SIGNUP" : "LOGIN"}
+        </Button>
+      </Card>
+    </React.Fragment>
+  );
+};
+
 export default Auth;
