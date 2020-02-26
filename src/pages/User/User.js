da124dcac43ea97022b5eb9e2eed4ecf82882274
jackdo69 <jackie.duc.do@gmail.com>
2020-02-26 19:45:57 +1100

    loaded image by user

--- src/pages/User/User.js
+++ src/pages/User/User.js
@@ -5,32 +5,57 @@ import LoadingSpinner from "../../components/LoadingSpinner/LoadingSpinner";
 import ErrorModal from "../../components/Modal/ErrorModal";
 import Card from "../../components/Card/Card";
 import Button from "../../components/Button/Button";
+import ImageItem from "../../components/Image/ImageItem";
 import { useHttpClient } from "../../hooks/http-hook";
 import { AuthContext } from "../../context/auth-context";
 import Tabs from "./Tabs";
 import Panel from "./Panel";
 import AddEditPhoto from "./AddEditPhoto";
+import Masonry from "react-masonry-css";
+
+//Masonry setup
+const breakpointColumnsObj = {
+  default: 3,
+  1100: 3,
+  700: 2,
+  500: 1
+};
 
 const User = () => {
   const auth = useContext(AuthContext);
   const userId = auth.userId;
   const [loadedUser, setLoadedUser] = useState();
+  const [uploadedPhotos, setUploadedPhotos] = useState();
   const [showAddEdit, setShowAddEdit] = useState(false);
   const { isLoading, error, sendRequest, clearError } = useHttpClient();
 
   useEffect(() => {
     const fetchUser = async () => {
       try {
-        const responseData = await sendRequest(
+        let res = await sendRequest(
           `http://localhost:5000/api/users/${userId}`
         );
-        setLoadedUser(responseData.user);
+        setLoadedUser(res.user);
       } catch (err) {}
     };
+
+    const fetchUploadedPhotos = async () => {
+      try {
+        let res = await sendRequest(
+          `http://localhost:5000/api/photos/${userId}`
+        );
+        console.log(res);
+
+        setUploadedPhotos(res.photos);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
     if (userId) {
       fetchUser();
+      fetchUploadedPhotos();
     }
-    
   }, [sendRequest, userId]);
 
   if (isLoading) {
@@ -51,6 +76,22 @@ const User = () => {
     );
   }
 
+  let photosGrid;
+  if (uploadedPhotos) {
+    photosGrid = uploadedPhotos.map(photo => {
+      return (
+        <ImageItem
+          src={`http://localhost:5000/${photo.image}`}
+          alt={photo.name}
+          key={photo.id}
+          author={photo.creator}
+          description={photo.description}
+          name={photo.name}
+        />
+      );
+    });
+  }
+
   return (
     <React.Fragment>
       <ErrorModal error={error} onClear={clearError} />
@@ -69,9 +110,8 @@ const User = () => {
             </div>
             <div className="userInfo">
               <p>@{loadedUser.name}</p>
-              
-                <span className="introduction">{loadedUser.introduction}</span>
-              
+
+              <span className="introduction">{loadedUser.introduction}</span>
             </div>
             <div className="addPhoto">
               <Button onClick={() => setShowAddEdit(true)}>Add Photo</Button>
@@ -80,7 +120,15 @@ const User = () => {
         )}
         <hr />
         <Tabs selected={0}>
-          <Panel title="Uploaded">This is the first panel</Panel>
+          <Panel title="Uploaded">
+            <Masonry
+              breakpointCols={breakpointColumnsObj}
+              className="my-masonry-grid"
+              columnClassName="my-masonry-grid_column"
+            >
+              {photosGrid}
+            </Masonry>
+          </Panel>
           <Panel title="Collection">This is the second panel</Panel>
           <Panel title="Liked">This is the third panel</Panel>
         </Tabs>
